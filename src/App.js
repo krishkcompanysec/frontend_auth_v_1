4f12b5a1c1ed083d53df0c99c72a4b81e34ca965
krishkcompanysec <krishnaanilkuma007@gmail.com>
2022-04-04 16:04:29 +0530

    Commit with initial routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./login";
+import Signup from "./signup";
+import Admin from "./admin";
+//................................//
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+//................................//
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>   
     </div>
   );
 }
